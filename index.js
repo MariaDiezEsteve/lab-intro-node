33375ce9c2b58c5554bd5bbcc98f5db041bd90a9
MariaDiezEsteve <diezesteve@outlook.es>
2022-05-03 18:14:37 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,63 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);  
+    this.items.sort((a, b) => a - b)
+    this.length++
+  }
 
-  get(pos) {}
+  get(pos) {
+ 
+      let itemsPos = this.items.indexOf(pos)
+  
+      if(itemsPos === - 1){
+        throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos]
+    }
+  }
 
-  max() {}
+  max() {
 
-  min() {}
+   let maxNum = Math.max(...this.items)
 
-  sum() {}
+    if(this.length > 0){
+      return maxNum
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  avg() {}
+  min() {
+
+
+    if(this.length > 0){
+      return Math.min(...this.items)
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
+
+  sum() {
+      let sumTotal = this.items.reduce((preValue, currentValue) => preValue + currentValue, 0
+      );
+
+      return sumTotal
+  }
+
+  avg() {
+    let sumTotal = this.items.reduce((preValue, currentValue) => preValue + currentValue, 0
+    );
+      if(sumTotal > 0){
+        return sumTotal / this.length
+      } else {
+        throw new Error('EmptySortedList');
+      }
+  }
 }
 
 module.exports = SortedList;
